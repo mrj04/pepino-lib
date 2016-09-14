145119ed4ca76d53325e0b39576d1353c9b708e8
Camilo Rivera <camilo@riverapineda.com>
2016-09-14 12:20:47 -0600

    add deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('pre-test', function () {
 });
 
 gulp.task('bump', function () {
-    var version = "1.0." + (process.env.TRAVIS_BUILD_NUMBER || "1");
+    var version = "1.1." + (process.env.BUILDKITE_BUILD_NUMBER || "1");
     return gulp.src('./package.json')
         .pipe(bump({ version: version }))
         .pipe(gulp.dest('./'));
