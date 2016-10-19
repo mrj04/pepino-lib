72859cedf1b84ac7d22bb68c3385947b144206cf
William Avila <william.zx1@gmail.com>
2016-10-19 11:45:12 -0600

    Adding logs

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -5,34 +5,15 @@ buildkite-agent meta-data set "acklen:slack:channel" "pepino"
 echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
 nvm current
-nvm use 4
-echo "--- NPM Login"
+nvm use 4 | tee bk-pipeline.log
+echo "--- NPM Login"  | tee bk-pipeline.log
 npm login <<!
 $NPM_USERNAME
 $NPM_PASSWORD
 $NPM_EMAIL
-!
-echo "--- Install Dependencies"
-npm update
-echo "--- Build And Test"
-gulp bump
-gulp
-if [[ "$BUILDKITE_BRANCH" == "develop"  ]]; then
-  echo "--- deploy to develop"
-  npm publish
-  cd ..
-  rm -rf pepino-demo
-  git clone git@github.com:AcklenAvenue/pepino-demo.git
-  cd pepino-demo
-  git commit --allow-empty -m "empty commit"
-  heroku git:remote -a pepino-demo
-  git push heroku develop:master
-fi
-if [[ "$BUILDKITE_BRANCH" == "staging"  ]]; then
-  echo "--- deploy to staging"
-  npm publish
-fi
-if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
-  echo "--- deploy to production"
-  npm publish
-fi
+! | tee bk-pipeline.log
+echo "--- Install Dependencies" | tee bk-pipeline.log
+yarn | tee bk-pipeline.log
+echo "--- Build And Test" | tee bk-pipeline.log
+gulp bump | tee bk-pipeline.log
+gulp | tee bk-pipeline.log
