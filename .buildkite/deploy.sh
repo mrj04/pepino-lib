c2bff56a7219fda41f3eb4b7ac536da77c93518a
Camilo Rivera <camilo@riverapineda.com>
2016-10-19 15:46:29 -0600

    fix push pepino demo

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -11,6 +11,6 @@ cd .. | tee bk-pipeline.log
 rm -rf pepino-demo | tee bk-pipeline.log
 git clone git@github.com:AcklenAvenue/pepino-demo.git | tee bk-pipeline.log
 cd pepino-demo | tee bk-pipeline.log
-git commit --allow-empty -m "empty commit" | tee bk-pipeline.log
+git reset HEAD~ | tee bk-pipeline.log
 heroku git:remote -a pepino-demo | tee bk-pipeline.log
-git push heroku develop:master | tee bk-pipeline.log
+git push -f heroku develop:master | tee bk-pipeline.log
