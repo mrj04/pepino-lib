42bd720845f5a2898e0f0eaa53b85b4bfc36369a
Camilo Rivera <camilo@riverapineda.com>
2016-10-19 14:46:48 -0600

    add nvm exprot to deploy

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 buildkite-agent meta-data set "acklen:slack:channel" "pepino"
 echo "--- Set Node Version"
 . "$NVM_DIR/nvm.sh"
-nvm current
 nvm use 4 | tee bk-pipeline.log
 echo "--- NPM Login"  | tee bk-pipeline.log
 npm login <<!
@@ -15,6 +14,4 @@ $NPM_EMAIL
 echo "--- Install Dependencies" | tee bk-pipeline.log
 yarn | tee bk-pipeline.log
 echo "--- Build And Test" | tee bk-pipeline.log
-gulp bump | tee bk-pipeline.log
 gulp | tee bk-pipeline.log
-npm publish
--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 set -eo pipefail
-#npm publish
+echo "--- Set Node Version"
+. "$NVM_DIR/nvm.sh"
+nvm use 4 | tee bk-pipeline.log
+echo "--- deploy to $BUILDKITE_BRANCH" | tee bk-pipeline.log
+gulp bump | tee bk-pipeline.log
+npm publish | tee bk-pipeline.log
+cd .. | tee bk-pipeline.log
+rm -rf pepino-demo | tee bk-pipeline.log
+git clone git@github.com:AcklenAvenue/pepino-demo.git | tee bk-pipeline.log
+cd pepino-demo | tee bk-pipeline.log
+git commit --allow-empty -m "empty commit" | tee bk-pipeline.log
+heroku git:remote -a pepino-demo | tee bk-pipeline.log
+git push heroku develop:master | tee bk-pipeline.log
