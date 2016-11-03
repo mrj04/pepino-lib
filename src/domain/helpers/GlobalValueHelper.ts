80ede24d250623576195bf4c4c15039fdb56a280
Jacob Ramirez <jacobramirez@Jacobs-MacBook-Pro.local>
2016-11-03 10:53:09 -0600

    Allowing uppercase and lowercase in global value

--- src/domain/helpers/GlobalValueHelper.ts
+++ src/domain/helpers/GlobalValueHelper.ts
@@ -5,6 +5,17 @@ export class GlobalValueHelper {
     static generate(text: string): string {
         var paramKeys: any = StringHelper.extractTextInQuotes(text);
         var field: string = paramKeys[0].toLowerCase().trim();
+
+        var lowercase = text.toLowerCase();
+
+        if (lowercase.indexOf('lowercase') !== -1) {
+            return "globalValues['" + field + "'].toString().toLowerCase()";
+        }
+
+        if (lowercase.indexOf('uppercase') !== -1) {
+            return "globalValues['" + field + "'].toString().toUpperCase()";
+        }
+
         return "globalValues['" + field + "']";
     }
 }
